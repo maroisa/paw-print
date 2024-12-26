d8ebd62b5dcff2428ad59459c7621b9fb205b331
maroisa <ahmadroisul412@gmail.com>
2024-12-26 21:43:12 +0700

    move health HUD to global HUD

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -8,6 +8,8 @@ var center_offset: Vector2
 onready var animstate = $AnimationTree.get("parameters/playback")
 onready var machine = get_node("PawStateMachine")
 
+func _ready(): HUD.refresh()
+
 func _physics_process(delta):
 	$AnimationTree.set("parameters/Idle/blend_position", velocity_static)
 	$AnimationTree.set("parameters/Walk/blend_position", velocity_static)
@@ -15,11 +17,10 @@ func _physics_process(delta):
 	center_offset = (get_viewport().get_mouse_position() - get_viewport_rect().get_center())
 	$Camera2D.offset = lerp(get_node("Camera2D").offset, center_offset / 8, 0.1)
 	$Pivot.position = lerp(get_node("Pivot").position, center_offset.normalized() * 100, 0.25)
-	
-	$CanvasLayer/M/ProgressBar.value = self.health
 
 func damaged(damage: int):
 	.damaged(damage)
+	HUD.refresh()
 	
 	machine.travel("Hit")
 
--- globals/hud.gd
+++ globals/hud.gd
@@ -3,6 +3,15 @@ extends CanvasLayer
 var cursor_cross = preload("res://assets/cursor_cross.png")
 var cursor_move = preload("res://assets/cursor_move.png")
 
+var player
+
 func _ready():
 	Input.set_custom_mouse_cursor(cursor_cross, Input.CURSOR_ARROW, Vector2(32, 32))
 	Input.set_custom_mouse_cursor(cursor_move, Input.CURSOR_MOVE, Vector2(32, 32))
+	
+	player = get_tree().root.get_node("World/Player")
+
+func refresh():
+	if !player: return
+	
+	$M/ProgressBar.value = player.health
